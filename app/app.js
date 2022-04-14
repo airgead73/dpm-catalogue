2b212d39a91f60392341782847b5e858cd1a922e
Dan Moneypenny <catullus1973@gmail.com>
2022-04-14 14:13:42 -0700

    add middleware

--- app/app.js
+++ app/app.js
@@ -2,7 +2,7 @@
  * external imports
  */
  const express = require('express');
- //const { auth } = require('express-openid-connect');
+ const { auth } = require('express-openid-connect');
  const path = require('path');
  const cors = require('cors');
  const cookieParser = require('cookie-parser');
@@ -14,22 +14,27 @@
 /**
  * internal imports
  */
+const { isDev } = require('./config/env');
+const { handleError } = require('./middleware');
+const { authConfig, connectDB, helmetPolicies, limiter, sessionConfig } = require('./config');
 
 /**
  * app activation
  */
 const app = express();
+connectDB();
+app.use(auth(authConfig));
 
 /**
  * security
  */
  app.use(helmet({ crossOriginEmbedderPolicy: false }));
- //app.use(helmet.contentSecurityPolicy(helmetPolicies));
+ app.use(helmet.contentSecurityPolicy(helmetPolicies));
  app.use(xss());
  app.use(hpp());
  app.use(cors());
  app.use(mongoSanitize());
- //app.use(limiter);
+ app.use(limiter);
 
 /**
  * middleware
@@ -38,7 +43,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(cookieParser());
-//app.use(session(sessionConfig));
+app.use(session(sessionConfig));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
@@ -56,6 +61,14 @@ app.get('/', (req, res, next) => {
 /**
  * error handling
  */
+ app.use(function(req, res, next) {
+  const error = new Error('Path not found.');
+  error.status = 404;
+  error.response = 'html'
+  next(error);
+});
+
+app.use(handleError);
 
 /**
  * export app
